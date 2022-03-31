d4013c2eb29b57106d384c67c00e75c99e2a8cb3
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2022-03-31 22:03:26 +0200

    mappen structuur

---
+++ t3/hoofdstuk6/6.Sessions/oefeningForms.php
@@ -0,0 +1,22 @@
+<!--Noah Beij
+    29/03/2022
+-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="oefeningForms-resultaat.php" method="post">
+    <input type="text" name="title">
+    <textarea name="message" cols="30" rows="10"></textarea>
+    <input type="number" name="amount">
+    <input type="checkbox" name="voorwaarden">
+    <input type="submit" value="Verzend📩">
+</form>
+</body>
+</html>
